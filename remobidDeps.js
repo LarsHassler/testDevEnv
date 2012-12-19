78d80a0d63a8fa8acf9a3cd83fb6f7d4535d57b9
LarsHassler <lars.hassler@explosive-marketing.de>
2012-12-20 00:07:23 +0100

    enabled real recursive file collecting

--- remobidDeps.js
+++ remobidDeps.js
@@ -1,3 +1,6 @@
 
 goog.addDependency('../../../../../../devEnv/testRunner.js', ['remobid.devEnv.TestRunner'], ['goog.array']);
 goog.addDependency('../../../../../../src/model.js', ['remobid.Model'], ['goog.events.EventTarget']);
+goog.addDependency('../../../../../../src/common/storage/storage_interface.js', ['remobid.common.storage.StorageInterface'], []);
+goog.addDependency('../../../../../../src/common/storage/storageErrorType_enum.js', ['remobid.common.storage.StorageErrorType'], []);
+goog.addDependency('../../../../../../src/common/storage/localstorage.js', ['remobid.common.storage.LocalStorage'], ['remobid.common.storage.StorageInterface']);
--- runAllTests.js
+++ runAllTests.js
@@ -10,15 +10,42 @@ var testRunner = require('./devEnv/testRunner.js'),
     TestRunner = new testRunner.testRunner(),
     fs = require('fs');
 
-fs.readdirSync('src').filter(function(file) {
-  // Only keep the .js files
-  return file.substr(-3) === '.js';
+var getSourceFiles = function(dir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
 
-}).forEach(function(file) {
-    // Use the method "addFile" to add the file to mocha
-    TestRunner.addFiles(file);
-});
+    var pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(file) {
+      file = dir + '/' + file;
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          getSourceFiles(file, function(err, res) {
+            results = results.concat(res);
+            if (!--pending) {
+              done(null, results);
+            }
+          });
+        } else {
+          if(file.substr(-3) === '.js' && file.substr(-13) !== '_interface.js')
+            results.push(file);
+          if (!--pending) {
+            done(null, results);
+          }
+        }
+      });
+    });
+  });
+};
 
-TestRunner.run(function() {
-  console.log('finished');
-});
+
+(function(){
+ getSourceFiles('test', function(err, files){
+   for(var i = 0, len = files.length; i < len; i++)
+     TestRunner.addFiles(files[i].substr(5));
+   TestRunner.run(function() {
+     console.log('finished');
+   });
+ });
+})();
