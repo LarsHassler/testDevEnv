4c1a4fb798991d46efc9f2e73314039bd837996d
LarsHassler <lars.hassler@explosive-marketing.de>
2012-12-29 10:46:43 +0100

    next step
    * Authorization
    * added handling multi connection to dispose

--- src/common/net/restmanager.js
+++ src/common/net/restmanager.js
@@ -4,6 +4,7 @@
 
 goog.provide('remobid.common.net.RestManager');
 
+goog.require('goog.crypt.base64');
 goog.require('goog.net.XhrManager');
 
 /**
@@ -14,22 +15,87 @@ goog.require('goog.net.XhrManager');
 remobid.common.net.RestManager = function(baseUrl) {
   goog.base(this);
 
+  /**
+   * the base url to use with the restManager.
+   * @type {string}
+   * @private
+   */
   this.baseUrl_ = baseUrl;
+
+  /**
+   * holds all global headers for this service rest.
+   * @type {object.<string, string>?}
+   * @private
+   */
+  this.headers_ = {};
+
+  /**
+   * stores the current state of the connection to the rest endpoint.
+   * @type {boolean}
+   * @private
+   */
+  this.available_ = false;
 };
 goog.inherits(remobid.common.net.RestManager, goog.net.XhrManager);
 
 /** @override */
 remobid.common.net.RestManager.prototype.dispose = function() {
+  remobid.common.net.RestManager.instanceConnections_[this.baseUrl_]--;
+  if (remobid.common.net.RestManager.instanceConnections_[this.baseUrl_] > 0) {
+    return;
+  }
+
   goog.base(this, 'dispose');
   delete remobid.common.net.RestManager.instances_[this.baseUrl_];
+  delete remobid.common.net.RestManager.instanceConnections_[this.baseUrl_];
 };
+
+/**
+ * adds header information for basic authentication
+ * @param {string=} opt_username username for authentication.
+ * @param {string=} opt_password password for authentication.
+ */
+remobid.common.net.RestManager.prototype.setBasicAuthentication = function(
+  opt_username, opt_password) {
+  var string2encode = opt_username + ':' + opt_password;
+  this.headers_['Authorization'] =
+      'Basic ' + goog.crypt.base64.encodeString(string2encode);
+};
+
+/**
+ * adds header information for oAuth authentication with bearer token
+ * @param {string} token the access token.
+ */
+remobid.common.net.RestManager.prototype.setBearerToken = function(token) {
+  this.headers_['Authorization'] =
+    'bearer ' + token;
+};
+
+/**
+ * @return {boolean} the status of the connection to the rest endpoint.
+ */
+remobid.common.net.RestManager.prototype.isAvailable = function() {
+  return this.available_;
+};
+
+
+
+// static
 
 /**
  * holds all instances of the RestManager.
  * @type {object.<string, remobid.common.net.RestManager>?}
  * @private
  */
-remobid.common.net.RestManager.instances_ = null;
+remobid.common.net.RestManager.instances_;
+
+/**
+ *
+ * holds the ussage counter for each instance
+ * @type {object.<string, number>?}
+ * @private
+ */
+remobid.common.net.RestManager.instanceConnections_;
 
 /**
  * base url to use if no url was provided.
@@ -48,12 +114,19 @@ remobid.common.net.RestManager.defaultBase_ = 'https://api.remobid.com';
 remobid.common.net.RestManager.getInstance = function(opt_baseUrl) {
   var baseUrl = opt_baseUrl || remobid.common.net.RestManager.defaultBase_;
 
-  if (!remobid.common.net.RestManager.instances_)
+  if (!remobid.common.net.RestManager.instances_) {
     remobid.common.net.RestManager.instances_ = {};
+    remobid.common.net.RestManager.instanceConnections_ = {};
+  }
 
-  if (!remobid.common.net.RestManager.instances_[baseUrl])
+
+  if (!remobid.common.net.RestManager.instances_[baseUrl]) {
     remobid.common.net.RestManager.instances_[baseUrl] =
       new remobid.common.net.RestManager(baseUrl);
+    remobid.common.net.RestManager.instanceConnections_[baseUrl] = 0;
+  }
+
+  remobid.common.net.RestManager.instanceConnections_[baseUrl]++;
 
   return remobid.common.net.RestManager.instances_[baseUrl];
 };
--- test/unit/common/net/restmanager.js
+++ test/unit/common/net/restmanager.js
@@ -32,6 +32,7 @@ describe('UNIT - restmanager', function () {
       var Manager = remobid.common.net.RestManager.getInstance();
       assertNotNull(remobid.common.net.RestManager.instances_);
       assertEquals(
+        'default base url is wrong',
         remobid.common.net.RestManager.defaultBase_,
         Manager.baseUrl_
       );
@@ -43,19 +44,23 @@ describe('UNIT - restmanager', function () {
       var Manager = remobid.common.net.RestManager.getInstance();
       assertNotNull(remobid.common.net.RestManager.instances_);
       assertEquals(
+        'default base url is wrong',
         remobid.common.net.RestManager.defaultBase_,
         Manager.baseUrl_
       );
       var Manager2 = remobid.common.net.RestManager.getInstance('test');
       assertEquals(
+        'wrong count of instances created',
         2,
         goog.object.getKeys(remobid.common.net.RestManager.instances_).length
       );
       assertEquals(
+        'default base url is wrong',
         remobid.common.net.RestManager.defaultBase_,
         Manager.baseUrl_
       );
       assertEquals(
+        'given base url is wrong',
         'test',
         Manager2.baseUrl_
       );
@@ -64,7 +69,65 @@ describe('UNIT - restmanager', function () {
     it('should be deleted if disposed', function() {
       var Manager = remobid.common.net.RestManager.getInstance();
       Manager.dispose();
-      assertObjectEquals({}, remobid.common.net.RestManager.instances_);
+      assertTrue(
+        'parent function was not called',
+        Manager.disposed_
+      );
+      assertObjectEquals(
+        'reference to the manager was not deleted',
+        {},
+        remobid.common.net.RestManager.instances_
+      );
+    });
+
+    it('should not be disposed if ' +
+        'there are still open connections', function() {
+      var Manager = remobid.common.net.RestManager.getInstance();
+      var Manager2 = remobid.common.net.RestManager.getInstance();
+      Manager.dispose();
+      assertFalse(
+        'should not be disposed yet',
+        Manager.disposed_
+      );
+      assertEquals('should not be deleted yet',
+        1,
+        goog.object.getKeys(remobid.common.net.RestManager.instances_).length);
+
+      Manager2.dispose();
+      assertTrue(
+        'Manager was not disposed properly',
+        Manager.disposed_
+      );
+      assertObjectEquals(
+        'reference to the manager was not deleted',
+        {},
+        remobid.common.net.RestManager.instances_
+      );
+    });
+
+  });
+
+  describe('settings', function() {
+
+    it('should store basic authentication', function() {
+      var Manager = remobid.common.net.RestManager.getInstance();
+      Manager.setBasicAuthentication('Aladdin', 'open sesame');
+      assertEquals(
+        'credentials are not base64 encoded',
+        'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==',
+        Manager.headers_['Authorization']
+      );
+    });
+
+    it('should store oAuth bearer token authentication', function() {
+      var Manager = remobid.common.net.RestManager.getInstance();
+      var token = 'e72e16c7e42f292c6912e7710c838347ae178b4a';
+      Manager.setBearerToken(token);
+      assertEquals(
+        'wrong header format',
+        'bearer e72e16c7e42f292c6912e7710c838347ae178b4a',
+        Manager.headers_['Authorization']
+      );
     });
 
   });
