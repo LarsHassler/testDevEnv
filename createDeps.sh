ccadea780cf337a048f0672eecf54ed9652e7a5e
LarsHassler <lars.hassler@explosive-marketing.de>
2013-01-07 17:31:23 +0100

    fixed path to closure library

--- createDeps.sh
+++ createDeps.sh
@@ -1,2 +1,6 @@
 
-../c4ms/closure/Library/closure/bin/build/depswriter.py --root_with_prefix="src ../../../../src"  --root_with_prefix="test ../../../../test" --root_with_prefix="devEnv ../../../../devEnv" > remobidDeps.js
+deps/closure-library/closure/bin/build/depswriter.py \
+  --root_with_prefix="src ../../../../src"  \
+  --root_with_prefix="test ../../../../test" \
+  --root_with_prefix="devEnv ../../../../devEnv" \
+  > remobidDeps.js
