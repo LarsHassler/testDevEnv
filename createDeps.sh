3b70148f06ee22defebb1dc3f6da71a50ed00e4f
LarsHassler <lars.hassler@explosive-marketing.de>
2013-01-07 18:13:39 +0100

    updated create scripts
    * added create template script
    * updated create deps script
    * moved create deps script

--- createDeps.sh
+++
@@ -1,6 +0,0 @@
-
-deps/closure-library/closure/bin/build/depswriter.py \
-  --root_with_prefix="src ../../../../src"  \
-  --root_with_prefix="test ../../../../test" \
-  --root_with_prefix="devEnv ../../../../devEnv" \
-  > remobidDeps.js
---
+++ devEnv/createDeps.sh
@@ -0,0 +1,8 @@
+
+../deps/closure-library/closure/bin/build/depswriter.py \
+  --root_with_prefix="../src ../../../../src"  \
+  --root_with_prefix="../test ../../../../test" \
+  --root_with_prefix="../templates/_compiled ../../../../templates/_compiled" \
+  --root_with_prefix="../devEnv ../../../../devEnv" \
+  --root_with_prefix="../deps/soy ../../../../deps/soy" \
+  > ../remobidDeps.js
