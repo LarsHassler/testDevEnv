a014208020a3eee6660e7f3d962a6a69eb2a3bef
alexanderTilg <alexander.tilg@explosive-marketing.de>
2013-01-10 17:32:28 +0100

    initial commint

---
+++ src/common/error/baseError.js
@@ -0,0 +1,33 @@
+/** @preserveTry */
+try {
+  if (require)
+    require('nclosure');
+} catch (e) {}
+
+goog.provide('remobid.common.error.BaseError');
+
+goog.require('goog.debug.Error');
+
+/**
+ * remobit base class for custom error objects.
+ * @param {string} errorType
+ * @param {*=} opt_msg The message associated with the error.
+ * @constructor
+ * @extends {goog.debug.Error}
+ */
+remobid.common.error.BaseError = function(errorType, opt_msg) {
+
+  /**
+   *
+   * @type {string}
+   */
+  this.errorType = errorType;
+
+  goog.base(this, opt_msg);
+
+}
+
+goog.inherits(remobid.common.error.BaseError, goog.debug.Error);
+
+/** @override */
+remobid.common.error.BaseError.prototype.name = 'RemobidError';
