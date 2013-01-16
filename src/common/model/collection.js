2393e1414008ff889e747ec3593ae51e41e63734
LarsHassler <lars.hassler@explosive-marketing.de>
2013-01-16 23:04:59 +0100

    added add functionality

---
+++ src/common/model/collection.js
@@ -0,0 +1,106 @@
+/**
+ * @fileoverview
+ */
+
+goog.provide('remobid.common.model.Collection');
+goog.provide('remobid.common.model.collection.Event');
+goog.provide('remobid.common.model.collection.EventType');
+
+goog.require('goog.asserts');
+goog.require('goog.events.Event');
+goog.require('remobid.common.model.Base');
+
+/**
+ * @extends {remobid.common.model.Base}
+ * @constructor
+ */
+remobid.common.model.Collection = function() {
+  goog.base(this);
+
+  /**
+   * holds all objects of this collection
+   * @type {Object.<remobid.common.model.ModelBase>}
+   * @private
+   */
+  this.items_ = {};
+};
+goog.inherits(remobid.common.model.Collection,
+  remobid.common.model.Base);
+
+/**
+ * @override
+ */
+remobid.common.model.Collection.prototype.disposeInternal = function() {
+  goog.base(this, 'disposeInternal');
+  goog.object.forEach(this.items_, function(item) {
+    item.dispose();
+  });
+  this.items_ = null;
+};
+
+/**
+ * adds an instance of remobid.common.model.ModelBase to the collection
+ * the Item has to have an identifier set. Dispatches an {@CODE ADDED} Event
+ * @param {remobid.common.model.ModelBase} item
+ *    the item to add.
+ */
+remobid.common.model.Collection.prototype.addItem = function(item) {
+  if(!(item instanceof remobid.common.model.ModelBase))
+    throw Error('invalid type');
+
+  if (!item.getIdentifier())
+     throw Error('keine ID');
+
+  goog.object.add(this.items_, item.getIdentifier(), item);
+  item.increaseReferenceCounter();
+  var event = new remobid.common.model.collection.Event(
+    remobid.common.model.collection.EventType.ADDED,
+    item
+  );
+  this.dispatchEvent(event);
+};
+
+/**
+ * @param {remobid.common.model.ModelBase} item
+ *    the item to check against the collection.
+ * @return {boolean} whenever the item is already in this collection.
+ */
+remobid.common.model.Collection.prototype.contains = function(item) {
+  return goog.object.containsKey(this.items_, item.getIdentifier());
+};
+
+/**
+ * a class of an special collection event which additionally hold the affected
+ * item
+ * @param {remobid.common.model.collection.EventType} type
+ *    the event type.
+ * @param {remobid.common.model.ModelBase} item
+ *    the item affected by the event.
+ * @extends {goog.event.Event}
+ * @constructor
+ */
+remobid.common.model.collection.Event = function(type, item) {
+  goog.base(this, type);
+
+  /**
+   * holds the affected item
+   * @type {remobid.common.model.ModelBase}
+   * @private
+   */
+  this.item_ = item;
+};
+goog.inherits(remobid.common.model.collection.Event,
+  goog.events.Event);
+
+/**
+ * @return {remobid.common.model.ModelBase} the item affected by the event.
+ */
+remobid.common.model.collection.Event.prototype.getItem = function() {
+  return this.item_;
+};
+
+/** @enum {string} */
+remobid.common.model.collection.EventType = {
+  DELETED: 'item deleted',
+  ADDED: 'item added'
+};
