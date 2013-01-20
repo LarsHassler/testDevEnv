1514ddb65481019b33ab4ce9fb1fe3d251a8df1e
LarsHassler <lars.hassler@explosive-marketing.de>
2013-01-20 11:59:26 +0100

    style(ui/control): renamed files which had uppercase as first letter

---
+++ src/common/ui/control/controlBaseRenderer.js
@@ -0,0 +1,204 @@
+/**
+ * @fileoverview base class for base control rendering.
+ */
+
+goog.provide('remobid.common.ui.control.ControlBaseRenderer');
+goog.provide('remobid.common.ui.control.controlBaseRenderer.ErrorType');
+goog.provide('remobid.common.ui.control.controlBaseRenderer.bindMethods');
+
+goog.require('goog.soy');
+goog.require('goog.ui.ControlRenderer');
+goog.require('remobid.common.ui.control.controlBase.Mapping');
+goog.require('remobid.templates.test');
+
+
+/**
+ * @constructor
+ * @extends {goog.ui.ControlRenderer}
+ */
+remobid.common.ui.control.ControlBaseRenderer = function() {
+  goog.base(this);
+
+  /**
+   * the template to use with this renderer.
+   * @type {function}
+   * @private
+   */
+  this.template_ = remobid.templates.test.modelDisplay;
+};
+goog.inherits(remobid.common.ui.control.ControlBaseRenderer,
+  goog.ui.ControlRenderer);
+goog.addSingletonGetter(remobid.common.ui.control.ControlBaseRenderer);
+
+/**
+ * overrides the template of this renderer.
+ * @param {function} template
+ *    the new template.
+ */
+remobid.common.ui.control.ControlBaseRenderer.prototype.setTemplate = function(
+    template) {
+  this.template_ = template;
+};
+
+/**
+ * @param {remobid.common.ui.control.ControlBase} control the control to render.
+ * @return {!Node} the HTML element for the control.
+ */
+remobid.common.ui.control.ControlBaseRenderer.prototype.createDom = function(
+    control) {
+  var templateData, element;
+
+  templateData = {
+    modelData: control.getModelData()
+  };
+  element = goog.soy.renderAsFragment(this.template_,
+    templateData);
+
+  this.setAriaStates(control, element);
+  return element;
+};
+
+/**
+ * parses the template and set the bindOptions to the given Control. Need to be
+ * called just one time.
+ * @param {Node} element the element of a control.
+ * @param {Object.<remobid.common.ui.control.controlBase.Mapping>} mappings the
+ *    attribute mappings of a control.
+ * @return {Object.<string, Array>} the bindings found in the html code.
+ */
+remobid.common.ui.control.ControlBaseRenderer.prototype.parseBinding = function(
+    element, mappings) {
+  var nodes2bind, bindings;
+
+  nodes2bind = goog.dom.findNodes(element, function(node) {
+    return node.nodeType != goog.dom.NodeType.TEXT &&
+      (goog.dom.dataset.has(node, 'rbBindGet') ||
+        goog.dom.dataset.has(node, 'rbBindSet'));
+  });
+  // also check the root node, since findNodes only checks the children of a
+  // given node
+  if (goog.dom.dataset.has(element, 'rbBindGet') ||
+      goog.dom.dataset.has(element, 'rbBindSet'))
+    nodes2bind.push(element);
+
+  bindings = {};
+
+  goog.array.forEach(
+    nodes2bind,
+    goog.bind(this.parseNode_, this, mappings, bindings)
+  );
+
+  return bindings;
+};
+
+/**
+ * parses one given dom node for all binding attributes. Also checks against
+ * the given mappings if the binding contains invalid data.
+ * @param {Object} mappings the mappings to check against for invalid data.
+ * @param {Object} bindings the final bindings object to put all new found
+ *    bindings into.
+ * @param {Node} node the node to check.
+ * @private
+ */
+remobid.common.ui.control.ControlBaseRenderer.prototype.parseNode_ =
+    function(mappings, bindings, node) {
+  var bindValues;
+
+  bindValues = goog.dom.dataset.get(node, 'rbBindGet').split('|');
+  goog.array.forEach(bindValues, function(values) {
+    if (values == '')
+      return;
+
+    var bindOptions = values.split(',');
+    var name = bindOptions[0];
+    var mapping = goog.object.findValue(mappings, function(mapping) {
+      return mapping.name == name;
+    });
+
+    // an unknown mapping name was found
+    if (!mapping) {
+      // todo change to remobid error instance
+      errorTypes = remobid.common.ui.control.controlBaseRenderer.ErrorType;
+      throw new Error(errorTypes.UNKNOWN_BINDING_NAME);
+      return;
+    }
+
+    var method = bindOptions[1].split(':');
+    // an unknown method was found
+    if (!goog.object.containsKey(
+          remobid.common.ui.control.controlBaseRenderer.bindMethods,
+          method[0])) {
+      // todo change to remobid error instance
+      var errorTypes = remobid.common.ui.control.controlBaseRenderer.ErrorType;
+      throw new Error(errorTypes.UNKNOWN_BINDING_METHOD);
+      return;
+    }
+
+    if (!goog.object.containsKey(bindings, name)) {
+      bindings[name] = [];
+    }
+    bindings[name].push({
+      mappings: mapping,
+      method: remobid.common.ui.control.controlBaseRenderer.bindMethods[method],
+      element: node,
+      control: method === 'control' ? true : null,
+      useHelper: bindOptions[2] === '1',
+      markParent: bindOptions[3] === '1',
+      classForExternal: !!bindOptions[4] ? bindOptions[4] : null
+    });
+
+  });
+};
+
+/**
+ * applies all binding to the appropriate elements
+ * @param {string} attribute
+ *    the attribute of the model which was changed.
+ * @param {boolean} external
+ *    whenever the the change was triggered from external.
+ * @param {Object.<string, Array>} bindOptions
+ *    all bind options of the control.
+ * @param {remobid.common.ui.control.ControlBase} control
+ *    the control.
+ */
+remobid.common.ui.control.ControlBaseRenderer.prototype.handleChangeEvent =
+    function(attribute, external, bindOptions, control) {
+  bindOptions = bindOptions[attribute];
+  if (!bindOptions)
+    return;
+
+  goog.array.forEach(bindOptions, function(options) {
+    var value = options.mappings.getter.call(control.getModel());
+    if (options.useHelper && goog.isDef(options.mappings.getterHelper)) {
+      value = options.mappings.getterHelper(value);
+    }
+    options.method(value, options.element, options.control);
+  });
+
+
+};
+//
+
+/**
+ * holds all valid functions that can be called via the binding
+ * @type {Object.<string, function>}
+ */
+remobid.common.ui.control.controlBaseRenderer.bindMethods = {
+  'html': function(value, element, control) {
+    element.innerHTML = value;
+  },
+  'text': function(value, element, control) {
+    goog.dom.setTextContent(element, value);
+  },
+  'tglClass': goog.nullFunction,
+  'chAttr': goog.nullFunction,
+  'control': goog.nullFunction
+};
+
+/** @enum {string} */
+remobid.common.ui.control.controlBaseRenderer.ErrorType = {
+  // if the template tries to bind to an unknown attribute of the model
+  UNKNOWN_BINDING_NAME: 'unknown_name',
+  // if the template tries to bind with an unknown method
+  UNKNOWN_BINDING_METHOD: 'unknown_method'
+};
---
+++ test/unit/browser/common/ui/control/controlBaseRenderer.js
@@ -0,0 +1,222 @@
+/**
+ * @fileoverview Tests for the base control renderer.
+ */
+
+if (typeof module !== 'undefined' && module.exports)
+  require('nclosure');
+
+goog.require('goog.dom');
+goog.require('goog.dom.dataset');
+goog.require('goog.testing.asserts');
+goog.require('remobid.common.ui.control.ControlBase');
+goog.require('remobid.common.ui.control.controlBase.Mappings');
+goog.require('remobid.common.ui.control.ControlBaseRenderer');
+goog.require('remobid.test.mock.Utilities');
+
+describe('UNIT - ControlBaseRenderer - ', function() {
+  var renderer, control, model, sandBox, testTemplate;
+
+  testTemplate = function(opt_data, opt_ignored) {
+    return '<div><span data-rb-bind-get="href,html,1,0|id,text,1,0">' +
+        '</span></div>';
+  };
+
+  before(function() {
+    sandBox = goog.dom.getElement('sandBox');
+  });
+
+  beforeEach(function(done) {
+    renderer = remobid.common.ui.control.ControlBaseRenderer.getInstance();
+    renderer.setTemplate(testTemplate);
+    model = new remobid.common.model.ModelBase('aada');
+    model.setAutoStore(false);
+    control = new remobid.common.ui.control.ControlBase(model);
+    remobid.test.mock.Utilities.clearStack(done);
+  });
+
+  after(function() {
+    // let the GC remove the instance of the Renderer since it is not longer
+    // needed but saved in the global scope
+    remobid.common.ui.control.ControlBaseRenderer.instance_ = null;
+  });
+
+
+  describe('binding - ', function() {
+
+    it('should parse all binding', function() {
+      var element, binding;
+      element = renderer.createDom(control);
+      binding = assertNotThrows('valid bindings not accept',
+        goog.bind(renderer.parseBinding, renderer, element, control.mappings_)
+      );
+
+      var expectedBindingAttributes = ['href', 'id'];
+
+      assertArrayEquals('wrong attributes parsed',
+        expectedBindingAttributes,
+        goog.object.getKeys(binding)
+      );
+    });
+
+    it('should not accept unknown attributes to bind', function() {
+      var element, exception;
+      element = renderer.createDom(control);
+      goog.dom.dataset.set(element, 'rbBindGet', 'unknown,html,1,0');
+      exception = assertThrows('unknown attribute accepted',
+        goog.bind(renderer.parseBinding, renderer, element, control.mappings_)
+      );
+
+      var errorType = remobid.common.ui.control.controlBaseRenderer.ErrorType;
+      assertEquals('wrong exception thrown',
+        errorType.UNKNOWN_BINDING_NAME,
+        exception.message
+      );
+    });
+
+
+    it('should not accept unknown methods', function() {
+      var element, exception;
+      element = renderer.createDom(control);
+      goog.dom.dataset.set(
+        element.children[0],
+        'rbBindGet',
+        'href,lalaFunction,1,0'
+      );
+      exception = assertThrows('unknown method accepted',
+        goog.bind(renderer.parseBinding, renderer, element, control.mappings_)
+      );
+
+      var errorType = remobid.common.ui.control.controlBaseRenderer.ErrorType;
+      assertEquals('wrong exception thrown',
+        errorType.UNKNOWN_BINDING_METHOD,
+        exception.message
+      );
+    });
+
+    it('should accept all known methods', function() {
+      var element = renderer.createDom(control);
+      var binding = 'href,html,1,0|' +
+          'href,text,1,0|' +
+          'href,tglClass:rb-classname,1,0|' +
+          'href,chAttr:title,1,0|' +
+          'href,control,1,0';
+      goog.dom.dataset.set(element.children[0], 'rbBindGet', binding);
+      var bindingOptions = assertNotThrows('known method not accepted',
+        goog.bind(renderer.parseBinding, renderer, element, control.mappings_)
+      );
+      assertEquals('wrong binding count for HREF',
+        5,
+        bindingOptions['href'].length
+      );
+    });
+
+    it('should return a full object for the binding options', function() {
+      var element = renderer.createDom(control);
+      var binding = 'href,html,1,0';
+      goog.dom.dataset.set(element.children[0], 'rbBindGet', binding);
+      var bindingOptions = renderer.parseBinding(element, control.mappings_);
+
+      assertEquals('there should be 1 bindings for HREF',
+        1,
+        bindingOptions['href'].length
+      );
+
+      // check mapping
+      assertObjectEquals('wrong mappings bound',
+        remobid.common.ui.control.controlBase.Mappings.HREF,
+        bindingOptions['href'][0].mappings
+      );
+      // check method
+      assertEquals('wrong method bound',
+        remobid.common.ui.control.controlBaseRenderer.bindMethods.html,
+        bindingOptions['href'][0].method
+      );
+      // check element
+      assertEquals('wrong element bound',
+        element.children[0],
+        bindingOptions['href'][0].element
+      );
+      // check control
+      assertNull('should not bound to a control',
+        bindingOptions['href'][0].control
+      );
+      // check useHelper flag
+      assertTrue('wrong useHelper flag bound',
+        bindingOptions['href'][0].useHelper
+      );
+      // check markParent flag
+      assertFalse('wrong markParent flag bound',
+        bindingOptions['href'][0].markParent
+      );
+      // check css class for external updates
+      assertNull('should not have a css class bound',
+        bindingOptions['href'][0].classForExternal
+      );
+    });
+
+    it('should update without a helper function', function() {
+      var element = renderer.createDom(control);
+      var binding = 'href,text,0,0';
+      goog.dom.dataset.set(element.children[0], 'rbBindGet', binding);
+      var bindingOptions = renderer.parseBinding(element, control.mappings_);
+      model.restUrl_ = 'https://www.testtest.co.uk';
+      renderer.handleChangeEvent('href', false, bindingOptions, control);
+      assertEquals('wrong updates applied',
+        'https://www.testtest.co.uk',
+        goog.dom.getTextContent(element.children[0])
+      );
+    });
+
+    it('should update without a helper function', function() {
+      var helperCalled = false;
+      var element = renderer.createDom(control);
+      var binding = 'href,html,1,0';
+      goog.dom.dataset.set(element.children[0], 'rbBindGet', binding);
+      var bindingOptions = renderer.parseBinding(element, control.mappings_);
+      model.restUrl_ = 'https://www.testtest.co.uk';
+      bindingOptions['href'][0].mappings.getterHelper = function(value) {
+        helperCalled = true;
+        return '#' + value + '!';
+      };
+
+      renderer.handleChangeEvent('href', false, bindingOptions, control);
+
+      assertTrue('helper function not called',
+        helperCalled
+      );
+
+      assertEquals('wrong updates applied',
+        '#https://www.testtest.co.uk!',
+        element.children[0].innerHTML
+      );
+
+    });
+
+    it('should not call helper function if flag not set', function() {
+      var helperCalled = false;
+      var element = renderer.createDom(control);
+      var binding = 'href,html,0,0';
+      goog.dom.dataset.set(element.children[0], 'rbBindGet', binding);
+      var bindingOptions = renderer.parseBinding(element, control.mappings_);
+      model.restUrl_ = 'https://www.testtest.co.uk';
+      bindingOptions['href'][0].mappings.getterHelper = function(value) {
+        helperCalled = true;
+        return '#' + value + '!';
+      };
+
+      renderer.handleChangeEvent('href', false, bindingOptions, control);
+
+      assertFalse('helper function should not be called',
+        helperCalled
+      );
+
+      assertEquals('wrong updates applied',
+        'https://www.testtest.co.uk',
+        element.children[0].innerHTML
+      );
+
+    });
+
+  });
+
+});
